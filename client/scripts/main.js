5383a2b0830f64b0ee22201aa679bac9d36597c5
Adur <sumeet.adur@FT-MW4273.local>
2015-11-13 16:53:37 +0000

    Initial commit

--- client/scripts/main.js
+++ client/scripts/main.js
@@ -8,5 +8,63 @@ document.addEventListener('DOMContentLoaded', () => {
   // remove the 300ms tap delay on mobile browsers
   FastClick.attach(document.body);
 
-  // YOUR CODE HERE!
+  document.getElementById("jargon-form").addEventListener("submit", function(event) {
+
+  	let error = false;
+
+  	let wordInput = this.elements["entry.544003616"];
+  	let wordInputContainer = wordInput.parentNode;
+  	let wordErrorText = document.getElementsByClassName("o-forms-errortext entry.544003616")[0];
+
+  	let agreeCheckbox = this.elements["agree"];
+  	let agreeErrorText = document.getElementsByClassName("o-forms-errortext agree")[0];
+
+  	if(wordInput.value === "") {
+	  	event.preventDefault();
+  		error = true;
+  		wordError();
+  		wordInput.focus();
+  		wordInput.addEventListener("keyup", function() {
+  			if(this.value !== "") {
+  				wordValid();
+  			} else {
+  				wordError();
+  			}
+  		});
+  	}
+  	if(!agreeCheckbox.checked) {
+	  	event.preventDefault();
+  		error = true;
+  		agreeError();
+  		agreeCheckbox.addEventListener("change", function() {
+  			if(this.checked) {
+  				agreeValid();
+  			} else {
+  				agreeError();
+  			}
+  		});
+  	}
+  	if(!error) {
+  		wordValid();
+  		agreeValid();
+  	}
+
+  	function wordError() {
+  		wordInputContainer.className += " o-forms--error";
+  		wordErrorText.style.display = "block";
+  	}
+
+  	function wordValid() {
+  		wordInputContainer.className = "o-forms-group";
+  		wordErrorText.style.display = "none";
+  	}
+
+  	function agreeError() {
+  		agreeErrorText.style.display = "block";
+  	}
+
+  	function agreeValid() {
+  		agreeErrorText.style.display = "none";
+  	}
+  });
 });
